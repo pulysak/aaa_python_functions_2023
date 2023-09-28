23fbd2d1d1df376a41c69d9dc13a34ec00cb86d7
Pavel Lysak <pyulysak@avito.ru>
2023-09-28 12:08:13 +0300

    updated unpacking

--- arguments6_unpacking.py
+++ arguments6_unpacking.py
@@ -4,7 +4,8 @@ def hello_world(name: str, age: int) -> str:
     return f'Hello, world. My name is {name}, my age is {age}'
 
 
-foo = ('John', 55)
+foo = ['John', 55]
+hello_world(foo[0], foo[1])
 hello_world(*foo)
 hello_world('John', 55)
 
@@ -12,5 +13,6 @@ bar = {
     'name': 'John',
     'age': 55,
 }
+hello_world(name=bar['name'], age=bar['age'])
 hello_world(**bar)
 hello_world(name='John', age=55)
